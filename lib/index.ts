7c1796bd1c8402e919d7d427c01a467c8960ff2b
Roger Jäggi <roger.jaeggi@avrios.com>
2025-04-28 20:21:17 +0200

    feat: support on-missing-data option (#13)

--- lib/index.ts
+++ lib/index.ts
@@ -55,6 +55,17 @@ export interface DatadogMonitorOptions {
      * The threshold definitions
      */
     readonly thresholds?: DatadogMonitorThresholds;
+
+    /**
+     * Controls how groups or monitors are treated if an evaluation does not return any data points.
+     * 
+     * The default option results in different behavior depending on the monitor query type.
+     * For monitors using Count queries, an empty monitor evaluation is treated as 0 and is compared to the threshold conditions.
+     * For monitors using any query type other than Count, for example Gauge, Measure, or Rate, the monitor shows the last known status.
+     * 
+     * This option is only available for APM Trace Analytics, Audit Trail, CI, Error Tracking, Event, Logs, and RUM monitors.
+     */
+    readonly onMissingData?: 'default' | 'show_no_data' | 'show_and_notify_no_data' | 'resolve';
 }
 
 export interface DatadogMonitorThresholds {
@@ -107,7 +118,8 @@ export class DatadogMonitor extends Construct {
                             CriticalRecovery: props.options?.thresholds?.criticalRecovery,
                             Warning: props.options?.thresholds?.warning,
                             WarningRecovery: props.options?.thresholds?.warningRecovery,
-                        }
+                        },
+                        OnMissingData: props.options?.onMissingData,
                     } : undefined
                 },
                 props.additionalMonitorParams
